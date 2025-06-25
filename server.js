c7821e439cb8397e5a8919ddc3b9c44be4d562ea
Makee <ina0312@naver.com>
2025-06-25 16:36:35 +0900

    app.js axios로 작성

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:9000",
+    origin: ["http://127.0.0.1:9000", "http://127.0.0.1:5500"],
     methods: ["OPTIONS", "POST", "GET", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.text());
 
 app.options("*", (req, res) => res.sendStatus(204));
-app.get("/", (req, res) => res.status(200).json(data));
+app.get("/", (req, res) => res.status(200).send(data.message));
 app.post("/", (req, res) => {
   data.message = req.body;
   res.status(200).send(`받은 POST 데이터: ${req.body}`);
